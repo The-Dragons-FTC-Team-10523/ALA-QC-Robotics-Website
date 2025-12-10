b23bc896c625adc7f28657be8c5171411d67db0b
gamemanuel <themathnight@gmail.com>
2025-12-10 16:40:52 -0700

    added the wyvern of the 404 picture new

--- app/wyverns/page.tsx
+++ app/wyverns/page.tsx
@@ -3,7 +3,7 @@
 export default function wyverns() {
     return (
       <div className="bg-[url(https://hc-cdn.hel1.your-objectstorage.com/s/v3/03b8790cb605005640494600ec13115cad44c3f3_img.png)] w-full h-full">
-        <img src="/img.png" className="w-full" />
+        <img src="/img_1.png" className="w-full" />
       </div>
     );
 }
